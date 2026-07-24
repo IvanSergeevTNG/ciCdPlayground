6b887d34d42cfbace9b72ba7b7aa0119c51e4a4d
IvanSergeevTNG <ivan.sergeev@tngtech.com>
2026-07-24 09:04:30 +0200

    fill in information in main.js

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Max Mustermann",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Continuous integration, continuous deployment. Processes and tools for software projects to keep adding new updates, delivering the latest version of the app to users, and ensuring the qualtiy of the products."],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "GitLab pipelines",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["Learn about the state of the art best practices, including available options and specific techniques."],
   },
 });
